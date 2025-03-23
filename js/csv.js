846db27b8db438f0f70078370c7ac3a869df2a9d
camarm-dev <contact@camarm.dev>
2025-03-23 11:17:37 +0100

    fix: added data browsing utils

--- js/csv.js
+++ js/csv.js
@@ -13,3 +13,17 @@ function loadData() {
         })
     })
 }
+
+function getMaxPrice(data) {
+    return Math.max(...data.map(row => Number(row.Price)).filter(price => price))
+}
+
+function getMinPrice(data) {
+    return Math.min(...data.map(row => Number(row.Price)).filter(price => price))
+}
+
+function getCountries(data) {
+    const countries = data.map(row => row.Country)
+    // Remove duplicates
+    return countries.filter((item, index) => countries.indexOf(item) === index && item)
+}
