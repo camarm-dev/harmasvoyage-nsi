ede4473fca92748c78b309bf2f8377ef5dd950fd
camarm-dev <contact@camarm.dev>
2025-03-23 14:27:56 +0100

    fix: anhanced scrapping

--- scrap.py
+++ scrap.py
@@ -1,23 +1,25 @@
-import requests
 import csv
 from bs4 import BeautifulSoup
 
 def get_location(city_name: str):
     for city in cities:
         if city_name == city[1]:
-            return city[8], city[9]
+            return [city[8], city[9]]
+    return []
 
 def get_place_data(place: BeautifulSoup, number: int):
     city = place.find(attrs={"class": "City__Name"}).text.strip()
     country = place.find(attrs={"class": "Country__Name"}).text.strip()
     price = place.find(attrs={"class": "_ib0 _18 _igh _ial _iaj"}).text.replace("dès ", "").replace("€", "").strip()
     image = "https://www.kayak.fr" + place.find("img")["data-original"]
+    if image == "https://www.kayak.fr":
+        image = "https://content.r9cdn.net/res/images/horizon/explore/destination-placeholder.png?v=6f09568c3b085bb065962f42f3ff300b422a576d&cluster=5"
     fly_time = ""
     location = get_location(city)
     images = [image]
     content = ""
     identifier = f"{number:02}"
-    return city, country, [city, country, fly_time, ",".join(images), price, content, location, identifier]
+    return city, country, [city, country, fly_time, ",".join(images), price, content, ",".join(location), identifier]
 
 def get_data():
     print("Opening kayak.html...")
@@ -26,10 +28,10 @@ def get_data():
     places = tree.find_all(attrs={"class": "Explore-GridViewItem"})
 
     all_places = []
-    all_places.append(["City", "Country", "Fly time", "Images", "Price", "Description", "Location", "Identifier"])
+    all_places.append(["City", "Country", "FlyTime", "Images", "Price", "Description", "Location", "Identifier"])
     for index, place in enumerate(places):
         city, country, data = get_place_data(place, index + 1)
-        print(f"[+] {data[-1]} {city} {country} {data[-2]}")
+        print(f"[+] {data[-1]} {city} {country} ({data[-2]})")
         all_places.append(data)
 
     print(f"Saving {len(all_places)} places to data.csv")
