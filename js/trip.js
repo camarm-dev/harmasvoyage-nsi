f1037f6d135bee87f0a34e526c4341da5baaf8d7
camarm-dev <contact@camarm.dev>
2025-03-23 15:46:55 +0100

    fix: like behaviour for trip

--- js/trip.js
+++ js/trip.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
+    const tripId = new URLSearchParams(window.location.search).get('id').trim()
+    const liked = getSavedTrips().includes(tripId)
+
     const citiesElements = document.querySelectorAll(".city")
     const countriesElements = document.querySelectorAll(".country")
     const image = document.getElementById("image")
@@ -8,7 +11,28 @@ document.addEventListener('DOMContentLoaded', async () => {
     const closestPlaceFlyTime = document.getElementById("closestPlaceFlyTime")
     const description = document.getElementById("description")
 
-    const tripId = new URLSearchParams(window.location.search).get('id').trim()
+    const likeButton = document.getElementById("like")
+    likeButton.addEventListener("click", () => {
+        saveTrip(tripId)
+        const icon = likeButton.querySelector(".icon svg")
+        const prefix = icon.dataset.prefix
+        icon.dataset.prefix = prefix === "far" ? "fas" : "far"
+        icon.parentElement.classList.toggle("is-red")
+        const text = likeButton.querySelector("span")
+        if (text.innerText === "Ajouter à ma liste d'envies") {
+            text.innerText = "Retirer de ma liste d'envies"
+        } else {
+            text.innerText = "Ajouter à ma liste d'envies"
+        }
+    })
+    if (liked) {
+        const likeIcon = likeButton.querySelector(".icon svg")
+        const likeIconPrefix = likeIcon.dataset.prefix
+        likeIcon.dataset.prefix = likeIconPrefix === "far" ? "fas" : "far"
+        likeIcon.parentElement.classList.toggle("is-red")
+        likeButton.querySelector("span").innerText = "Retirer de ma liste d'envies"
+    }
+
     loadData().then(data => {
         const trip = data.find(row => row.Identifier === tripId)
 
