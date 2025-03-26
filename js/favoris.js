a90b2cb3a71aca62c0b7bbc3e479b16192d3cca3
Elias835 <harmaselias324@gmail.com>
2025-03-26 10:51:49 +0000

    favoris

--- js/favoris.js
+++ js/favoris.js
@@ -1,24 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let section = document.getElementById("favoris")
+    const section = document.getElementById("favoris")
     loadData().then(data => {
-        let fav = getSavedTrips()
-        for(let i of data) {
-            if (fav.includes(i.Identifier)){
-                console.log(i)
-                section.innerHTML += `<div class="card">
-            <img src="${i.Images.split(',')[0]}" alt="Image de ${i.City}">
-            <header>
-              <h3 class="title">${i.City}</h3>
-              <h3 class="title">${i.Country}</h3>
-              <h3 class="title">dès ${i.Price}€</h3>
-            </header>
-            <div class="content">
-              <p>${i.Country}</p>
-              <p>${i.FlyTime}</p>
-            </div></div>
-                `
+        const fav = getSavedTrips()
+        for (const i of data) {
+            if (fav.includes(i.Identifier)) {
+                const card = document.createElement("a") 
+                card.class = "card"
+                card.href = "trip.html?id=" + i.Identifier
+                card.innerHTML = `
+                <img src="${i.Images.split(',')[0]}" alt="Image de ${i.City}">
+                <header>
+                    <h3 class="title">${i.City}</h3>
+                    <h3 class="title">${i.Country}</h3>
+                    <h3 class="title">dès ${i.Price}€</h3>
+                </header>
+                <div class="content">
+                    <p>${i.Country}</p>
+                    <p>${i.FlyTime}</p>
+                </div>`
+                section.appendChild(card)
             }
-
         }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
     })
