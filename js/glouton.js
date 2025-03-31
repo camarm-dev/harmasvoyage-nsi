8be5ce5ddf0a83cd8e1077367280d2c2141ff66d
Elias835 <harmaselias324@gmail.com>
2025-03-31 07:50:38 +0000

    ,n

--- js/glouton.js
+++ js/glouton.js
@@ -1,16 +1,35 @@
 function getClosestPlace(startPlace, places) {
     // Il faut renvoyer le lieu le plus proche de "startPlace" parmit "places"
+    let plus_petite_distance = undefined
+    let destination_plus_proche = undefined
+    const [startLatitude, startLongitude] = startPlace.Location.split(",")
+    for (const destination of places) {
+        const [destinationLatitude, destinationLongitude] = destination.Location.split(",")
+        const distance = getDistance([Number(startLatitude), Number(startLongitude)], [Number(destinationLatitude), Number(destinationLongitude)])
+        if (plus_petite_distance > distance || plus_petite_distance == undefined) {
+            plus_petite_distance = distance
+            destination_plus_proche = destination
+        }
+    }
+    return destination_plus_proche
+    
 }
 
 // Trouver le chemin le plus court de startPlace, à endPlaces, en passant par places
 function getShortestWay(startPlace, endPlace, places) {
-    const [startLatitude, startLongitude] = startPlace.Location.split(",")
-    for (const destination of places) {
-        const [destinationLatitude, destinationLongitude] = destination.Location.split(",")
-        const distance = getDistance([Number(startLatitude), Number(startLongitude)],[Number(destinationLatitude),Number(destinationLongitude)])
-        console.log(distance)
+    const trip = [startPlace]
+    let depart = startPlace
+    while (places.length !== 0){
+        depart = getClosestPlace(depart, places)
+        const targetIndex = places.findIndex(el => el.Identifer === depart.Identifier)
+        places.splice(targetIndex, 1)
+        trip.push(depart)
     }
-}
+    trip.push(endPlace)
+    return trip
+
+}   
+
 // getDistance([latitude, longitude], [...])
 // trip.Location "34.04000000,-4.6531000"
 // const location = "34.04000000,-4.6531000".split(",")
