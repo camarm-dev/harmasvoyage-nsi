945bc0472fd2f077974852bfa06408ad1891b918
camarm-dev <contact@camarm.dev>
2025-04-05 18:07:16 +0200

    fix: refacto + endPlace could be undefined

--- js/glouton.js
+++ js/glouton.js
@@ -1,36 +1,45 @@
 function getClosestPlace(startPlace, places) {
     // Il faut renvoyer le lieu le plus proche de "startPlace" parmit "places"
-    let plus_petite_distance = undefined
-    let destination_plus_proche = undefined
+    let shortestDistance = undefined
+    let closestPlace = places[0]
     const [startLatitude, startLongitude] = startPlace.Location.split(",")
     for (const destination of places) {
         const [destinationLatitude, destinationLongitude] = destination.Location.split(",")
         const distance = getDistance([Number(startLatitude), Number(startLongitude)], [Number(destinationLatitude), Number(destinationLongitude)])
-        if (plus_petite_distance > distance || plus_petite_distance == undefined) {
-            plus_petite_distance = distance
-            destination_plus_proche = destination
+        if (shortestDistance > distance || shortestDistance == undefined) {
+            shortestDistance = distance
+            closestPlace = destination
         }
     }
-    destination_plus_proche.FlyTime = getFlyTime(plus_petite_distance)
-    return destination_plus_proche
-    
+    closestPlace.FlyTime = getFlyTime(shortestDistance)
+    return closestPlace
+
 }
 
 // Trouver le chemin le plus court de startPlace, à endPlaces, en passant par places
 function getShortestWay(startPlace, endPlace, steps) {
     const places = [...steps]
     const trip = [startPlace]
-    let depart = startPlace
+    let place = startPlace
     while (places.length !== 0) {
-        depart = getClosestPlace(depart, places)
-        const targetIndex = places.findIndex(el => el.Identifier === depart.Identifier)
+        place = getClosestPlace(place, places)
+        const targetIndex = places.findIndex(el => el.Identifier === place.Identifier)
         places.splice(targetIndex, 1)
-        trip.push(depart)
+        trip.push(place)
     }
-    trip.push(endPlace)
+    if (endPlace) {
+        const [lastPlaceLatitude, lastPlaceLongitude] = place.Location.split(",")
+        const [endPlaceLatitude, endPlaceLongitude] = endPlace.Location.split(",")
+        const lastStepDistance = getDistance(
+            [Number(lastPlaceLatitude), Number(lastPlaceLongitude)],
+            [Number(endPlaceLatitude), Number(endPlaceLongitude)]
+        )
+        endPlace.FlyTime = getFlyTime(lastStepDistance)
+        trip.push(endPlace)
+    }
     return trip
 
-}   
+}
 
 // getDistance([latitude, longitude], [...])
 // trip.Location "34.04000000,-4.6531000"
